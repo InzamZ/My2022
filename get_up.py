b3ff2f0f9ba6580fa94a7868ce3b65818bde3c33
InzamZ <zhengyinzhen2001@icloud.com>
2022-04-02 19:22:06 +0800

    修改部分逻辑

--- get_up.py
+++ get_up.py
@@ -7,7 +7,7 @@ from github import Github
 # 14 for test 12 real get up
 GET_UP_ISSUE_NUMBER = 1
 GET_UP_MESSAGE_TEMPLATE = (
-    "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦，喝杯咖啡，背个单词，去跑步。\r\n\r\n 今天的一句诗:\r\n {sentence}"
+    "今天的起床时间是--{get_up_time}.\r\n\r\n 起床啦，泡一下茶，背个单词。\r\n\r\n 今天的一句诗:\r\n {sentence}"
 )
 SENTENCE_API = "https://v1.jinrishici.com/all"
 DEFAULT_SENTENCE = "赏花归去马如飞\r\n去马如飞酒力微\r\n酒力微醒时已暮\r\n醒时已暮赏花归\r\n"
@@ -65,21 +65,20 @@ def main(github_token, repo_name, weather_message, tele_token, tele_chat_id):
     if weather_message:
         weather_message = f"现在的天气是{weather_message}\n"
         body = weather_message + early_message
-    if is_get_up_early:
-        issue.create_comment(body)
+    if not is_get_up_early:
+        body += "起晚了,要注意休息哦"
         # send to telegram
-        if tele_token and tele_chat_id:
-            requests.post(
-                url="https://api.telegram.org/bot{0}/{1}".format(
-                    tele_token, "sendMessage"
-                ),
-                data={
-                    "chat_id": tele_chat_id,
-                    "text": body,
-                },
-            )
-    else:
-        print("You wake up late")
+        # if tele_token and tele_chat_id:
+        #     requests.post(
+        #         url="https://api.telegram.org/bot{0}/{1}".format(
+        #             tele_token, "sendMessage"
+        #         ),
+        #         data={
+        #             "chat_id": tele_chat_id,
+        #             "text": body,
+        #         },
+        #     )
+    issue.create_comment(body)
 
 
 if __name__ == "__main__":
